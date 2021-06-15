315cb3eb09724109a8a921d03c28e200683e0215
Tammy Gegoski <tgegoski@gmail.com>
2021-06-15 12:07:59 -0400

    set up bookssearch.js and app.js files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.css";  
+import Country from './components/country';  
+import GoogleBooksSearch from "./googleBooksSearch";  
+  
+function App() {  
+  return (  
+    <GoogleBooksSearch></GoogleBooksSearch>  
+  )  
+}  
+  
+export default App;   
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h2>Welcome to React</h2>
+//       </div>
+//       <p className="App-intro">
+//         To get started, edit <code>src/App.js</code> and save to reload.
+//       </p>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-}
 
-
-export default App;
+// export default App;
