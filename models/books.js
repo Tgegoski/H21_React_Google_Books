0c71ff6f1369648a3f1436d163281e0e4775e9e4
Tammy Gegoski <tgegoski@gmail.com>
2021-06-16 02:45:11 -0400

    set up models

---
+++ models/books.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const booksSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+  },
+  author: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  date: {
+    type: Date,
+  }
+
+});
+
+const Books = mongoose.model("Books", booksSchema);
+
+module.exports = Books;
