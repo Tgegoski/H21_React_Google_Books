90025dbb9f6374ef21514d142dcfc8eb8ea53ff2
Tammy Gegoski <tgegoski@gmail.com>
2021-06-26 01:48:35 -0400

    moved and modified package.json to include correct name

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
 const path = require('path');
-const { useRouteMatch } = require('react-router-dom');
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+
+// const { useRouteMatch } = require('react-router-dom');
 
 router.use('/api', apiRoutes);
 
+
 router.use(function(req, res) { 
-    res.sendFile(path.join(_dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
